69a36451cbeca3b36093c774beda73ac4ebf0e5b
Marin <kuglermarinkevin@gmail.com>
2024-10-03 09:35:12 -0300

    commit

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { signup, login } from "../controllers/user-controller.js";
-import jwtAuthenticator from "../middlewares/jwt-authenticator.js";
+import authorizer from "../middlewares/authorizer.js";
+import authenticator from "../middlewares/authenticator.js"
 
 const router = Router();
 
-router.post("/signup", signup);
 router.post("/login", login)
+router.post("/signup", signup);
+
+router.use(authenticator)
+router.use(authorizer("ADMINISTRADOR"))
+
+// crud user
 
 export default router;
